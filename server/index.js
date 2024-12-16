040c963ef9dee9ada0baba45662a01fba08e3331
MateoPillajo <98783413+Mates182@users.noreply.github.com>
2024-12-16 17:26:46 -0500

    feat: :sparkles: Create a Service Queue

--- server/index.js
+++ server/index.js
@@ -1,33 +1,51 @@
-import express from 'express'
-import logger from 'morgan'
+import express from 'express';
+import logger from 'morgan';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import { createClient } from 'redis';
 
-import {Server} from 'socket.io'
-import {createServer} from 'node:http'
+const port = process.env.PORT ?? 3000;
+const app = express();
 
-const port = process.env.PORT ?? 3000
-const app = express()
+const server = createServer(app);
+const io = new Server(server);
 
-const server = createServer(app)
-const io = new Server(server)
+const redisClient = createClient();
+await redisClient.connect();
 
-io.on('connection', async (socket) =>{
-    console.log('a user has connected!')
+io.on('connection', (socket) => {
+  console.log('A user connected!');
 
-    socket.on('disconnect', () =>{
-        console.log('an user has disconnected')
-    })
+  // Emit stored messages from Redis
+  redisClient.lRange('chat_messages', 0, -1).then((messages) => {
+    messages.forEach((message) => {
+      const { username, msg } = JSON.parse(message);
+      socket.emit('chat message', msg, username);
+    });
+  });
 
-    socket.on('chat message', async (msg) =>{
-        io.emit('chat message', msg, socket.handshake.auth.username)
-    })
-})
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
 
-app.use(logger('dev'))
+  socket.on('chat message', async (msg) => {
+    const username = socket.handshake.auth.username;
+    const message = JSON.stringify({ username, msg });
+    
+    // Store message in Redis
+    await redisClient.rPush('chat_messages', message);
+
+    // Emit message to all connected clients
+    io.emit('chat message', msg, username);
+  });
+});
+
+app.use(logger('dev'));
 app.use(express.static('client'));
 app.get('/', (req, res) => {
-    res.sendFile(process.cwd() + '/client/index.html')
-})
+  res.sendFile(process.cwd() + '/client/index.html');
+});
 
 server.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
